58f0f058f76d76bd3fd9a9a1038bd2eaebbdd6e4
shayanmdh86 <shayanmdhy0@gmail.com>
2023-06-06 02:16:39 +0330

    Defind regex pattern and dev Addproduct method

--- HW5_ShayanMaddahi/Interface/ProductRepository.cs
+++ HW5_ShayanMaddahi/Interface/ProductRepository.cs
@@ -10,9 +10,20 @@ namespace HW5_ShayanMaddahi.Interface
 {
     public class ProductRepository : IProductRepository
     {
+        bool Test;
         public string AddProduct(Product product)
         {
+            CheckProductName(product.Name);
+            if (Test)
+            {
+                Console.WriteLine("ok");
+            }
+            else
+            {
+                Console.WriteLine("NOt ok");
+            }
 
+            return "done";
         }
 
         public string GetProductById(int id)
@@ -28,7 +39,7 @@ namespace HW5_ShayanMaddahi.Interface
         private bool CheckProductName(string name)
         {
             Regex regex = new Regex(@"^[A-Z][a-z]{3}[1-9]{1}[_]{1}[1-9]{2}$");
-            bool Test =regex.IsMatch(name);
+            Test =regex.IsMatch(name);
             if (!Test)
                 Console.WriteLine("product name is not correct");
 
--- HW5_ShayanMaddahi/Program.cs
+++ HW5_ShayanMaddahi/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace HW5_ShayanMaddahi
+﻿using HW5_ShayanMaddahi.Domain;
+using HW5_ShayanMaddahi.Interface;
+
+namespace HW5_ShayanMaddahi
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ProductRepository productrep = new ProductRepository();
+            Product product = new Product();
+
+            product.ProductId = 1;
+            product.Name = "Te14le1_Ab";
+            product.Barcode = "1";
+            productrep.AddProduct(product);
+
         }
     }
 }
